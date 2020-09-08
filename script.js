13d2f71fe697f8ca7ed1c1a46bb1628fe5594458
datracka <datracka@gmail.com>
2020-09-08 15:39:54 +0200

    fix stupid error

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 console.log("Hello World Tag", document.location.href);
 
-function mobilizeInit(uc, uf) {
-  console.log("Start...");
-  console.log(uc, uf);
-}
+(function bgInit() {
+  console.log("module pattern");
+  function mobilizeInit(uc, uf) {
+    console.log("Start...");
+    console.log(uc, uf);
+  }
+  return {
+    mobilizeInit,
+  };
+})();
