d243dbf0bf7952be6b9eaca07f7b56c2faf4a45a
datracka <datracka@gmail.com>
2020-09-08 16:57:33 +0200

    final skeleton

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 var mobilize = (function (w) {
-  function initMobilize(uc, uf) {
-    w.addEventListener("load2", () =>
-      jstag.getid((id) => console.log("id", id))
-    );
+  var UNIQUE_CAMPAIGN = uc;
+  var UNIQUE_FORM = uf;
+  var ENABLE_DEBUGGING = true;
+  var CONTAINER_ID = "form-container";
+
+  w.addEventListener("load", () =>
+    jstag.getid((id) => {
+      console.log("id", id, UNIQUE_CAMPAIGN, UNIQUE_FORM);
+    })
+  );
+  function init(uc, uf) {
+    UNIQUE_CAMPAIGN = uc;
+    UNIQUE_FORM = uf;
   }
-  return { initMobilize };
+  return { init };
 })(window);
