3cd874fc21be540a39fc0a1cfe3cb802e6fac4ba
datracka <datracka@gmail.com>
2020-09-08 15:37:24 +0200

    add log

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 console.log("Hello World Tag", document.location.href);
 
 (function bgInit() {
+  console.log("module pattern");
   function mobilizeInit(uc, uf) {
     console.log("Start...");
     console.log(uc, uf);
