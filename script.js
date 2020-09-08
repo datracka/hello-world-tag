e7dfda7c3331f6bc6fd5dc095da885d0c677620c
datracka <datracka@gmail.com>
2020-09-08 15:43:17 +0200

    move to es6

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 console.log("Hello World Tag", document.location.href);
 
-(function bgInit() {
-  console.log("module pattern");
-  function mobilizeInit(uc, uf) {
-    console.log("Start...");
-    console.log(uc, uf);
-  }
-  return {
-    mobilizeInit,
-  };
-})();
+const bgIinit = () => ({
+  mobilizeInit = (uc, uf) => console.log('uc, uf', uc, uf);
+}) 
+
+bgIinit()
