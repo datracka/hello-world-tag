a458e1becfd95b2517322dd0526e720ceac84a2a
datracka <datracka@gmail.com>
2020-09-08 16:52:25 +0200

    export module

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-(function (w) {
+var mobilize = (function (w) {
   w.addEventListener("load", () => jstag.getid((id) => console.log("id", id)));
   function initMobilize(uc, uf) {
     console.log("initMobilize", uc, uf);
-    w.addEventListener("load", () =>
-      jstag.getid((id) => console.log("id2", id))
-    );
   }
   return { initMobilize };
 })(window);
