f0eeda04dabaa8b8b1de045c63e21ef9175557be
datracka <datracka@gmail.com>
2020-09-08 15:44:51 +0200

    fix typo

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("Hello World Tag", document.location.href);
 
 const bgIinit = () => ({
-  mobilizeInit = (uc, uf) => console.log('uc, uf', uc, uf)
-}) 
+  mobilizeInit: (uc, uf) => console.log("uc, uf", uc, uf),
+});
 
-bgIinit()
+bgIinit();
