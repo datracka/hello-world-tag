c120f635a06f239c7ccb96aa543e7b8c7e70c0e8
datracka <datracka@gmail.com>
2020-09-10 12:30:30 +0200

    another more

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 var mobilize = (function (w) {
-  var UNIQUE_CAMPAIGN;
-  var UNIQUE_FORM;
   var ENABLE_DEBUGGING = true;
   var CONTAINER_ID = "form-container";
 
@@ -24,53 +22,52 @@ var mobilize = (function (w) {
     return urlparameter;
   }
 
-  w.addEventListener("load", () => {
-    try {
-      jstag.getid((id) => {
-        console.log("id", id, UNIQUE_CAMPAIGN, UNIQUE_FORM);
-        let numericPartOfUid = id.slice(2);
-        let utmTerm = getUrlParam("utm_term", "");
-        let utmSource = getUrlParam("utm_source", "");
-        let utmContent = getUrlParam("utm_content", "");
-        let utmCampaign = getUrlParam("utm_campaign", "");
-        let utmMedium = getUrlParam("utm_medium", "");
-        let firstName = getUrlParam("first_name", "");
-        let lastName = getUrlParam("last_name", "");
-        let email = getUrlParam("email", "");
-        let telephone = getUrlParam("telephone", "");
-
-        ENABLE_DEBUGGING
-          ? console.log(
-              "parameters to send to lytics",
-              `_uid=${numericPartOfUid} utm_campaign=${utmCampaign} utm_term=${utmTerm} utm_source=${utmSource} utm_content=${utmContent} utm_medium=${utmMedium}
-               first_name=${firstName} last_name=${lastName} email=${email} telephone=${telephone}`
-            )
-          : null;
-        // create iFrame and append to `div`
-        let d = document.getElementById(CONTAINER_ID);
-        let ifrm = document.createElement("iframe");
-        ifrm.setAttribute(
-          "src",
-          `https://takeaction.citizenwindow.io/?uc=${UNIQUE_CAMPAIGN}&uf=${UNIQUE_FORM}&_uid=${numericPartOfUid}&utm_term=${utmTerm}&utm_source=${utmSource}&utm_content=${utmContent}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&first_name=${firstName}&last_name=${lastName}&email=${email}&telephone=${telephone}`
-        );
-        ifrm.setAttribute("id", "advocates-iframe");
-        ifrm.setAttribute("width", "100%");
-        ifrm.setAttribute("height", "720px");
-        ifrm.setAttribute("allowtransparency", true);
-        ifrm.setAttribute("frameborder", "0");
-        ifrm.setAttribute("scrolling", "no");
-        d.appendChild(ifrm);
-      });
-    } catch (err) {
-      console.log("Lytics jstag not found!!", err);
-    }
-  });
   function init(uc, uf) {
-    UNIQUE_CAMPAIGN = uc;
-    UNIQUE_FORM = uf;
-    w.addEventListener("load", function () {
-      console.log("called from init");
+    var UNIQUE_CAMPAIGN = uc;
+    var UNIQUE_FORM = uf;
+
+    w.addEventListener("load", () => {
+      try {
+        jstag.getid((id) => {
+          console.log("id", id, UNIQUE_CAMPAIGN, UNIQUE_FORM);
+          let numericPartOfUid = id.slice(2);
+          let utmTerm = getUrlParam("utm_term", "");
+          let utmSource = getUrlParam("utm_source", "");
+          let utmContent = getUrlParam("utm_content", "");
+          let utmCampaign = getUrlParam("utm_campaign", "");
+          let utmMedium = getUrlParam("utm_medium", "");
+          let firstName = getUrlParam("first_name", "");
+          let lastName = getUrlParam("last_name", "");
+          let email = getUrlParam("email", "");
+          let telephone = getUrlParam("telephone", "");
+
+          ENABLE_DEBUGGING
+            ? console.log(
+                "parameters to send to lytics",
+                `_uid=${numericPartOfUid} utm_campaign=${utmCampaign} utm_term=${utmTerm} utm_source=${utmSource} utm_content=${utmContent} utm_medium=${utmMedium}
+                 first_name=${firstName} last_name=${lastName} email=${email} telephone=${telephone}`
+              )
+            : null;
+          // create iFrame and append to `div`
+          let d = document.getElementById(CONTAINER_ID);
+          let ifrm = document.createElement("iframe");
+          ifrm.setAttribute(
+            "src",
+            `https://takeaction.citizenwindow.io/?uc=${UNIQUE_CAMPAIGN}&uf=${UNIQUE_FORM}&_uid=${numericPartOfUid}&utm_term=${utmTerm}&utm_source=${utmSource}&utm_content=${utmContent}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&first_name=${firstName}&last_name=${lastName}&email=${email}&telephone=${telephone}`
+          );
+          ifrm.setAttribute("id", "advocates-iframe");
+          ifrm.setAttribute("width", "100%");
+          ifrm.setAttribute("height", "720px");
+          ifrm.setAttribute("allowtransparency", true);
+          ifrm.setAttribute("frameborder", "0");
+          ifrm.setAttribute("scrolling", "no");
+          d.appendChild(ifrm);
+        });
+      } catch (err) {
+        console.log("Lytics jstag not found!!", err);
+      }
     });
   }
+
   return { init };
 })(window);
